ee9ce218cf25785470488d2f7a91144a8f26bade
JuanInzunzaRivas <juanjose.inzunza.rivas@gmail.com>
2025-11-16 21:38:40 -0300

    Add files via upload

---
+++ waybar/rofi-power.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+all=([SHUTDOWN] [REBOOT] [LOGOUT] [LOCK])
+
+show=("${all[@]}")
+
+declare -A actions
+actions[SHUTDOWN]="poweroff"
+actions[REBOOT]="reboot"
+actions[LOGOUT]="hyprctl dispatch exit"
+actions[LOCK]="hyprlock"
+
+chosen=$(echo -e "[SHUTDOWN]\n[REBOOT]\n[LOGOUT]\n[LOCK]" | \
+rofi -dmenu -no-custom -theme ~/.config/rofi/signalis-power.rasi)
+
+selection="$chosen"
+
+case "$selection" in
+    "[SHUTDOWN]") ${actions[SHUTDOWN]} ;;
+    "[REBOOT]") ${actions[REBOOT]} ;;
+    "[LOGOUT]") ${actions[LOGOUT]} ;;
+    "[LOCK]") ${actions[LOCK]} ;;
+    *) exit 1 ;;
+esac
